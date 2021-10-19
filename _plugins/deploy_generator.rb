d93c2570558c02699a72d6c0d9b6513840b64e18
Benau <Benau@users.noreply.github.com>
2021-10-19 14:22:36 +0800

    Redirect non-translated pages properly

--- _plugins/deploy_generator.rb
+++ _plugins/deploy_generator.rb
@@ -2,6 +2,27 @@ module STKWebsite
     class DeployGenerator < Jekyll::Generator
         priority :highest
         def generate(site)
+            supported_languages = []
+            for lang in site.data['translations'] do
+                if lang[0] != 'en'
+                    supported_languages.push(lang[0])
+                end
+            end
+            page_translations = {}
+            for page in site.pages do
+                if File.extname(page.name) != '.md' then
+                    next
+                end
+                basename = File.basename(page.name, '.md')
+                lang = page.data['lang']
+                if lang and lang != 'en' then
+                    if page_translations.include?(basename) then
+                        page_translations[basename].push(lang)
+                    else
+                        page_translations[basename] = [ lang ]
+                    end
+                end
+            end
             for page in site.pages do
                 if File.extname(page.name) != '.md' then
                     next
@@ -17,6 +38,17 @@ module STKWebsite
                         end
                     elsif lang == 'en' then
                         page.data["redirect_from"] = [ '/en/' + basename]
+                    end
+                    if lang == 'en' then
+                        missing = supported_languages.dup
+                        if page_translations.include?(basename) then
+                            for page_translation in page_translations[basename] do
+                                missing.delete_if {|i| i == page_translation }
+                            end
+                        end
+                        for missing_translation in missing do
+                            page.data["redirect_from"].push(missing_translation + '/' + basename)
+                        end
                     end
                 end
             end
